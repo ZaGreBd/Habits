5272e76bdabb342b64b0c2ce9344a18f1fb84b18
ZaGreBd <pedrodindinho16@gmail.com>
2023-01-22 17:25:15 -0400

    Habits

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,25 @@
-const form = document.querySelector("#form-habits")
-const registerButton = document.getElementById("register-button")
-const nlwSetup = new NLWSetup(form)
+const form = document.querySelector("#form-habits");
+const registerButton = document.getElementById("register-button");
+const nlwSetup = new NLWSetup(form);
 
-registerButton.addEventListener("click", add)
-form.addEventListener("change", save)
+registerButton.addEventListener("click", add);
+form.addEventListener("change", save);
 
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
 
-function add(){
-  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
-
-  if(nlwSetup.dayExists(today)){
-    alert("O dia já foi existe!")
-  }else{
-    nlwSetup.addDay(today)
-    alert(`Dia ${today} registrado com sucesso!`)
+  if (nlwSetup.dayExists(today)) {
+    alert("O dia já foi existe!");
+  } else {
+    nlwSetup.addDay(today);
+    alert(`Dia ${today} registrado com sucesso!`);
   }
 }
 
-function save(){
-  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data));
 }
 
-const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
-nlwSetup.setData(data)
-nlwSetup.load()
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
+nlwSetup.setData(data);
+nlwSetup.load();
